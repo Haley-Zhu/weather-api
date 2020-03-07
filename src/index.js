40c25d712f7013e2ac1142b80eac988f97222f4f
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-03-07 23:25:30 +1100

    add routes, nodemon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("hhhhhhh");
-})
-
-app.get('/api/weather/:cc/:city', (req, res) => {
-  const { cc, city } = req.params;
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=e3ecd9546eaf7b421f60e9c4a1b22be1`)
-  .then(response => {res.send(response.data)})
-  .catch(err => console.log(err));
-})
+app.use(routes);
 
 app.listen(3000, () => console.log(`app listen on port 3000`));
