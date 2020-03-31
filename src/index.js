7fc4fd04ef0d173fac41bcac38ba20a83144542a
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-04-01 00:52:00 +1100

    Add notFound

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 require('dotenv').config();
 const routes = require('./routes');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const logger = require('./utils/logger');
+const notFoundHandler = require('./middlewares/notFound');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -14,5 +16,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('common'));
 }
 app.use(routes);
+app.use(notFoundHandler);
 
-app.listen(PORT, () => logger.log(`app listen on port ${PORT}`));
+app.listen(PORT, () => logger.info(`app listen on port ${PORT}`));
