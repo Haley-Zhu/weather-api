08e47b4634f716abaaf361cdb579691796ed4909
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-04-02 15:43:43 +1100

    add query weatherType

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const axios = require("../utils/axios");
+const weather = require("../models/Weather");
 
 const router = express.Router();
 
 router.get("/:cc/:city", (req, res) => {
   const { cc, city } = req.params;
-  axios
-    .get("/weather", { params: { q: `${city},${cc}` } })
+  const weatherType = req.query.weatherType;
+  weather
+    .getData(city, cc, weatherType)
     .then(response => {
-      res.send(response.data);
+      res.json(response);
     })
     .catch(err => console.log(err));
 });
